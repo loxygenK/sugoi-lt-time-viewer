441431502d107e0072664d160e934fce0e22e2ac
loxygenK <loxygen.k@gmail.com>
2020-11-22 00:14:47 +0900

    †追加† Captionのスタイルが微妙にミスってた

--- src/comps/detail/Caption.tsx
+++ src/comps/detail/Caption.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import styled from "styled-components";
 
-const CaptionRoot = styled.span`
+const CaptionRoot = styled.div`
+  display: inline-block;
   font-size: 2.5em;
   line-height: 1em;
   background: #4f4f69;
   color: #fff;
-  padding: 0.2em 2em;
+  padding: 0.5em 2em;
 `;
 
 export function Caption(props: { caption: string }): React.ReactElement {
