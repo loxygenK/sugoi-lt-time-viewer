779b704833c5f951308cfb225c9cb87a31e1ba3f
loxygenK <loxygen.k@gmail.com>
2020-11-22 00:35:35 +0900

    †修正† スタイルがちょっと違う

--- src/comps/PresentationInfo.tsx
+++ src/comps/PresentationInfo.tsx
@@ -2,12 +2,13 @@ import * as React from "react";
 import { Presentation } from "../types/Presentation";
 import styled from "styled-components";
 import { Caption } from "./detail/Caption";
+import { PresentationData } from "./detail/PresentationData";
 
 const PresentationInfoRoot = styled.div`
   font-family: "Noto Sans JP", sans-serif;
   font-weight: 900;
   padding: 5em 0;
-  color: #ffffff;
+  color: #4f4f69;
 `;
 
 export function PresentationInfo(props: { presentation: Presentation }): React.ReactElement {
