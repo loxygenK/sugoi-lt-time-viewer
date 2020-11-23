f0aeb7949a315019df73d0b1638c1f394aa5c127
loxygenK <loxygen.k@gmail.com>
2020-11-23 11:10:05 +0900

    †追加† ログを取るようにした＋コマンド実行失敗時の処理を追加

--- src/back/WebsocketListener.tsx
+++ src/back/WebsocketListener.tsx
@@ -1,29 +1,42 @@
+import * as Log from "../utils/Logger";
+
 export class WebSocketListener {
   private wsClient: WebSocket;
 
   constructor(portNumber: number) {
-    console.log("--> Generating WebSocket...");
     this.wsClient = new WebSocket(`ws://localhost:${portNumber}/`);
-    console.log("--> Registering EventListener...");
     this.wsClient.addEventListener("open", () => this.onConnected());
     this.wsClient.addEventListener("message", (e) => this.onMessage(e));
-    console.log("--> Completed generating WebSocketListener.");
   }
 
   private onConnected(): void {
-    console.log("Ok connected");
-    this.sendText("echo Hi client, let's do our best together!");
+    Log.Success("WSL-CON", "Successfully connected!\nRetrieving LT Information...");
+    this.requestLTInfo();
   }
 
   private onMessage(event: MessageEvent): void {
     const message = event.data.toString();
     if (message.startsWith("CLIENT|")) return;
-    console.log(`RX<< ${message}`);
-    //this.sendText(`Did you just said "${message}"?`);
+    Log.Success("WSL-MSG", "A message received!");
+    Log.Info("WSL-MSG", `RX<< ${message}`);
+
+    const body = message.substring(7).split(">>");
+    if (body[1].startsWith("?")) {
+      Log.Failure(
+        "WSL-MSG",
+        `The command execution failure has been reported.\nCommand : ${body[0]}\nResponse: ${body[1]}`,
+      );
+      return;
+    }
+  }
+
+  private requestLTInfo(): void {
+    Log.Info("WSL-RLT", "Requesting current LT information.");
+    this.sendText("RLT");
   }
 
   private sendText(text: string): void {
-    console.log(`TX>> ${text}`);
+    Log.Info("WSL-STX", `TX>> ${text}`);
     this.wsClient.send(`CLIENT|${text}`);
   }
 }
